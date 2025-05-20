91cd9f093e31a56c6b93fcd8c7aff4cfcb52c170
ILLUMINATQW <162901348+ILLUMINATQW@users.noreply.github.com>
2025-05-20 14:28:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Biologia',
+    'O que é célula Procarionte?',
+    'São células que possuem nucleo em seu interior'
+)
+
+criaCartao(
+    'Biologia',
+    'O que possibila as plantas a fazer fotossintese?',
+    'Os plastos, mais especificamnetes os cloroplastos que sua coloração é verde'
+)
+
+criaCartao(
+    'Biologia',
+    'Por que a Biologia existi?',
+    'Para entendermos os animais e seres que vivem em nosso ambiente e mostra como nós somos mais proximos do que imaginamos'
+)
+
+criaCartao(
+    'Biologia',
+    'Existe outras areas da Bilogia?Cite uma',
+    'Biotecnologia'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem fundou a biologia?',
+    'Charles Darwin'
+)
+
+criaCartao(
+    'Biologia',
+    'Por que nós somos parentes de todos os seres vivos do planeta?',
+    'Pois geneticamente e biologicamente temos coisas e requicios de caracteristicas de outros animais'
+)
+
